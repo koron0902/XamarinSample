bb25dea00b5bf6a551579253d5cfe7297414516a
Keita <koron0902@gmail.com>
2019-08-24 04:48:05 +0900

    `Broadcast Receiver` のサンプルを実装

---
+++ BroadcastReceiver/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Content;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace BroadcastReceiver {
+	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+	public class MainActivity : AppCompatActivity {
+		static TextView tv;
+
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+			SetContentView(Resource.Layout.activity_main);
+
+			tv = FindViewById<TextView>(Resource.Id.tv1);
+
+			receiver receiver = new receiver();
+			IntentFilter filter = new IntentFilter();
+			filter.AddAction(Intent.ActionBatteryChanged);
+			RegisterReceiver(receiver, filter);
+			
+		}
+
+		public static void Update(int _level) {
+			tv.Text = _level.ToString() + "%";
+		}
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
+			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
+}
+
